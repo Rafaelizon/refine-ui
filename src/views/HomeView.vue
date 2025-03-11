85a96aac88945c1c75de3f5a85afb144984cc1f1
Rafael Guaitanele Niszczak <RNiszczak@erastogaertner.com.br>
2025-03-11 16:12:38 -0300

    added listOrder store and HomeReportBox component and changes to the homview and authstore.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import RouterButton from '@/components/RouterButton.vue';
 import Section from '@/components/Section.vue';
+import HomeReportBox from '@/components/HomeReportBox.vue';
 
 </script>
 
 <template>
 
-  <Section title="Home" icon="house">
+  <Section title="Home" icon="house" class="flex-1">
     <div class="router-link-grid">
       <RouterButton to="/self-service" icon="utensils">
         Atendimento
@@ -17,12 +18,36 @@ import Section from '@/components/Section.vue';
       <RouterButton to="/reports" icon="clipboard-list">
         Relatorios
       </RouterButton>
-      <RouterButton to="/manual-service" icon="clipboard-list">
+      <RouterButton to="/manual-service" icon="mug-hot">
         Atendimento manual
       </RouterButton>
-
+      <RouterButton to="" icon="user" variant="danger">
+        Clientes
+      </RouterButton>
+      <RouterButton to="" icon="users" variant="danger">
+        Usuarios
+      </RouterButton>
+      <RouterButton to="" icon="credit-card" variant="danger">
+        Saldo Alimentação
+      </RouterButton>
+      <RouterButton to="" icon="xmark" variant="danger">
+        Remover Pedido
+      </RouterButton>
     </div>
   </Section>
+  <section>
+      <footer class="mt-auto bg-white-800 dark:bg-zinc-800 shadow-md">
+        <div class="router-link-grid p-4">
+          <HomeReportBox title="Clientes Cadastrados"/>
+          <HomeReportBox title="Usuarios Cadastrados"/>
+          <HomeReportBox title="Produtos Cadastrados"/>
+          <HomeReportBox title="Pedidos da Semana" :type="'WeekOrders'"/>
+          <HomeReportBox title="Total Em Vendas da Semana" />
+          <HomeReportBox title="Total Em Vendas do Mês"/>
+        </div>
+    </footer>
+  </section>
+
 </template>
 
 <style>
